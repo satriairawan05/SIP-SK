8e7d3e70f399446464f9d7b4f88c078e76e724bc
Satria Irawan <satriai418@gmail.com>
2023-10-24 10:44:15 +0800

    when access root redirect to Login

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-})->name('home-page');
+    return redirect('login');
+});
 
 Route::middleware('guest')->group(function () {
     // Login
